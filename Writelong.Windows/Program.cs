7b91fb2a9da30a3c8ed59afd2656b8853e292b5d
Heer12354 <heer@example.com>
2026-08-01 14:13:05 +0530

    Fix WinUI release publishing

--- Writelong.Windows/Program.cs
+++ Writelong.Windows/Program.cs
@@ -7,6 +7,7 @@ public static class Program
     [STAThread]
     public static void Main()
     {
+        Environment.SetEnvironmentVariable("MICROSOFT_WINDOWSAPPRUNTIME_BASE_DIRECTORY", AppContext.BaseDirectory);
         Application.Start(_ => new App());
     }
 }
